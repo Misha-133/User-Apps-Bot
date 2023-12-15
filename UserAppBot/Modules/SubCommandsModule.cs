07e82a032e6e551f8fa4e84586286ffbc472a3d8
Misha133 <mihagribkov133@gmail.com>
2023-12-15 10:04:07 +0300

    Create SubCommandsModule.cs

---
+++ UserAppBot/Modules/SubCommandsModule.cs
@@ -0,0 +1,19 @@
+﻿namespace UserAppBot.Modules;
+
+[CommandContextType(ApplicationCommandContextType.BotDm, ApplicationCommandContextType.PrivateChannel, ApplicationCommandContextType.Guild)]
+[IntegrationType(ApplicationIntegrationType.UserInstall)]
+[Group("rp", "rp")]
+public class SubCommandsModule : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("bonk", "bonk some1")]
+    public async Task BonkCommand(IUser user)
+    {
+        await RespondAsync($"# [BONK](https://tenor.com/view/vorzek-vorzneck-oglg-og-lol-gang-gif-24901093) {user.Mention}", allowedMentions: AllowedMentions.None);
+    }
+
+    [SlashCommand("slap", "slap some1")]
+    public async Task SlapCommand(IUser user)
+    {
+        await RespondAsync($"Slap {user.Mention}", allowedMentions: AllowedMentions.None);
+    }
+}
